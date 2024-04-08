0aa09c9f683ae60745fefe56c88948766cb18fca
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-04-08 19:56:28 +0900

    implemented store management  as MVP

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,2 +1,32 @@
 class StoresController < ApplicationController
+  def index
+    @stores = Store.all.order("created_at DESC")
+  end
+  
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.create(store_params)
+    redirect_to stores_path
+  end
+  
+  def edit
+    @store= Store.find(params[:id])
+  end
+
+  def update
+    @store = Store.find(params[:id])
+    if @store.update(store_params)
+      redirect_to stores_path
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  private
+  def store_params
+    params.require(:store).permit(:store_name, :description, :address, :latitude, :longitude, :opening_hours)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'try_on_for_client/index'
+
   root to: 'items#index'
   resources :items, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :stores
+  resources :stores, only: [:index, :new, :create, :edit, :update, :destroy]
 end
