189329f340879d7a807a13d061f23d9eb1575a85
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-04-11 11:29:27 +0900

    implemented edit user info

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,7 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys:[:first_name, :last_name, :store_id])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:first_name, :last_name, :store_id])
   end
 
 end
