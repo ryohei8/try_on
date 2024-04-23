e0b6fd2008b786843262c7d352d1fa3e12c07e60
Ryohei Hachisuga <ryohei.8suga@gmail.com>
2024-04-23 19:47:11 +0900

    implemented filtering by category

--- app/controllers/tryonforclient_controller.rb
+++ app/controllers/tryonforclient_controller.rb
@@ -8,8 +8,9 @@ class TryonforclientController < ApplicationController
   def search
     return render json: { error: "商品名が指定されていません。" }, status: 400 if params[:q][:item_name_cont].blank?
     search_result = @q.result.includes(:store)
-    stores_with_inventory = search_result.select{ |item| item.number_of_inventory > 3 }.map(&:store)
+    
 
+    stores_with_inventory = search_result.select{ |item| item.number_of_inventory > 3 }.map(&:store)
     markers = stores_with_inventory.map do |store|
       {
         lat: store.latitude,
